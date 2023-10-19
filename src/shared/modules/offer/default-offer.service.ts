78c920ae83cb34beeb49c3acf2158e37909926f9
Dias <dison08@mail.ru>
2023-10-20 00:38:19 +0600

    дом задание 5.2

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -5,6 +5,9 @@ import { Logger } from '../../libs/logger/index.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { PREMIUM_OFFER_COUNT } from './offer.constant.js';
+import { SortType } from '../../types/sort-type.enum.js';
 
 @injectable()
 export class DefaultOfferService implements OfferService {
@@ -21,6 +24,77 @@ export class DefaultOfferService implements OfferService {
   }
 
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel
+      .findById(offerId)
+      .populate(['author'])
+      .exec();
+  }
+
+  public async find(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentsCounts: 1,
+      }}).exec();
+  }
+
+  public async findPremium(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find({ isPremium: true})
+      .sort({ createdAt: SortType.Down })
+      .limit(PREMIUM_OFFER_COUNT)
+      .exec();
+  }
+
+  public async findFavorites(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find({ isFavorite: true})
+      .exec();
+  }
+
+  public async getOfferRating(offerId: string): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .aggregate([
+        {
+          $lookup: {
+            from: 'comments',
+            let: { offerId: '$_id'},
+            pipeline: [
+              { $match: { offerId } },
+              { $project: { rating: 1}}
+            ],
+            as: 'offerComments'
+          },
+        },
+        { $addFields:
+          { rating: { $avg: '$offerComments'} }
+        },
+        { $unset: 'comments' },
+      ]).exec();
+
   }
 }
---
+++ src/shared/modules/user/dto/update-user.dto.ts
@@ -0,0 +1,8 @@
+import { UserType } from '../../../types/index.js';
+
+export class UpdateUserDto {
+  public email?: string;
+  public avatar?: string;
+  public name?: string;
+  public type: UserType;
+}
